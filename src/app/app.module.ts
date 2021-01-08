cdf256f9923522f9ae119d829b71dba99e608c99
rogermedico <roger.medico@gmail.com>
2021-01-08 15:27:44 +0100

    ssr through angular universal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,7 @@ import { environment } from '../environments/environment';
     WordComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
--- src/app/services/last-searches.service.ts
+++ src/app/services/last-searches.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -8,12 +9,16 @@ export class LastSearchesService {
   _searches: string[] = [];
   maxSearches: number = 10;
 
-  constructor() { }
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
 
   load(): string[] {
-    const lastSearches = JSON.parse(localStorage.getItem('encyclopediaLastSearches'));
-    if (lastSearches == null) this._searches = [];
-    else this._searches = lastSearches;
+
+    if (isPlatformBrowser(this.platformId)) {
+      const lastSearches = JSON.parse(localStorage.getItem('encyclopediaLastSearches'));
+      if (lastSearches == null) this._searches = [];
+      else this._searches = lastSearches;
+    }
+
     return this._searches;
   }
 
@@ -25,7 +30,7 @@ export class LastSearchesService {
     if (!this.load().includes(search)) {
       this._searches.unshift(search)
       if (this._searches.length > this.maxSearches) this._searches.splice(-1, 1);
-      localStorage.setItem('encyclopediaLastSearches', JSON.stringify(this._searches));
+      if (isPlatformBrowser(this.platformId)) localStorage.setItem('encyclopediaLastSearches', JSON.stringify(this._searches));
     }
   }
 }
